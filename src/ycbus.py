8626230c43ef2773308150c629321a82a194cd97
BrianHsu <brian.hsu0702@gmail.com>
2021-06-04 19:17:01 +0800

    update code

--- src/ycbus.py
+++ src/ycbus.py
@@ -1,5 +1,9 @@
 import os
 import re
+import logging
+import datetime
+import time
+from logging.config import fileConfig
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,14 +11,24 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
+
+from os import path
+log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging_config.ini')
+logging.config.fileConfig(log_file_path)
+# fileConfig('logging_config.ini')
+my_debug_log = logging.getLogger(__name__).debug
 
 
 class AutoReserve:
     def __init__(self):
         # User Data
+        self.xpath = None
         self.data = dict()
         data_file = os.getcwd() + "\\ycbus_data.txt"
-        with open(data_file, 'r') as file:
+        with open(data_file, 'r', encoding="utf-8") as file:
             f_read = file.read()
 
         split_symbol = '[=|\n]'
@@ -41,18 +55,22 @@ class AutoReserve:
         self.data['back_off_city'] = re.split(split_symbol, f_read)[31]
         self.data['back_off_area'] = re.split(split_symbol, f_read)[33]
         self.data['back_off_address'] = re.split(split_symbol, f_read)[35]
-        
+
         # CSS Element
         self.css = dict()
         self.css['customerName'] = "input#cusname.w3-large"
         self.css['idCode'] = "input#idcode.w3-large"
         self.css['loginButton'] = "input#btn101.btn"
-        self.css['checkNumber'] = "#chknumber"
-        self.css['sendButton'] = "#next5"
+        self.css['checkNumber'] = "input#chknumber"
+        self.css['checkMyID'] = "form#form1 h1:nth-child(4)"
+        self.css['backMain'] = 'form#form1 div:nth-child(2) > input[name="btn1"]'
+
+        self.css['sendButton'] = "input#next5"
         self.css['dateButton'] = 'input[type=button][value*="%s"]' % self.data['date']
-        self.css['setGoButton'] = "#setgom2"
+        self.css['setGoButton'] = "input#setgom2"
+        #                           input[type=radio][onclick*="go_time"]
         self.css['goTimeButton'] = 'input[type=radio][onclick*="%s"]' % self.data['go_time']
-        self.css['setBackButton'] = "input[name='setgon']"
+        self.css['setBackButton'] = "input#setgon"
         self.css['backTimeButton'] = 'input[type=radio][onclick*="%s"]' % self.data['back_time']
         # goOn Address
         self.css['goOnAreaIn'] = "input[name='areain']"
@@ -76,17 +94,18 @@ class AutoReserve:
         self.css['backOffCitySelect'] = "select[name='citym']"
         self.css['backOffAreaSelect'] = "select[name='areaoffb_u']"
         self.css['backOffAddress'] = "input[type=text][name='pointoff2']"
-
-        # xpath Element
-        self.xpath = dict()
-        self.xpath['bookingButton'] = "//tr[@id='chktmf']//input[@id='uya']"
+        # time clock
+        self.css['timeClock'] = "span#time"
+        # booking
+        self.css['bookingButton'] = "tr#chktmf input#uya"
 
         # webdriver
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('disable-gpu')
         chrome_options.add_argument('--start-maximized')
         chrome_options.add_argument('--enable-application-cache')
-        self.driver = webdriver.Chrome(options=chrome_options)  # 启动时添加定制的选项
+        # self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(options=chrome_options, executable_path=ChromeDriverManager().install())  # 启动时添加定制的选项
         try:
             self.driver.get('http://rayman.ycbus.org.tw/rayman/book_inq.php')
         except WebDriverException:
@@ -119,35 +138,151 @@ class AutoReserve:
                     print('%s is Timeout' % element_name)
                     print('Fail try %s count' % i)
 
+    def loop_now_time(self):
+        status = 1
+        while status == 1:
+            time.sleep(1)
+            time_get = self.driver.find_element_by_css_selector(self.css['timeClock']).text
+            regex = re.compile(r'\d{4}-\d{2}-\d{2} (\d{2}:\d{2}):(\d{2})')
+            search_time = regex.search(time_get)
+            debug = 0
+            lock_time = "07:00"
+            try:
+                if search_time.group(1) == lock_time and int(search_time.group(2)) > 0:
+                    print("時間到: %s" % lock_time)
+                    status = 0
+                elif debug == 1:
+                    status = 0
+                else:
+                    print("尚未到 %s, 現在時間: [%s]" % (lock_time, search_time.group(0)))
+            except:
+                pass
+
+        self.reserve()
+
+    def check_enter(self):
+        try:
+            WebDriverWait(self.driver, 0.5).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "form#form1 div > h1:nth-child(1)"))
+            )
+            self.reserve()
+        except TimeoutException:
+            print("不用 再輸入乘客編號\n")
+
     def login(self):
         try:
             self.wait_element('customerName').send_keys(self.data['name'])
             self.wait_element('idCode').send_keys(self.data['num'])
-            self.wait_element('loginButton').click()
+            (self.wait_element('loginButton')).click()
         except AttributeError as attrErr:
             print(attrErr)
             exit("The login function error.")
 
+    def wait_load(self):
+        wait = 1
+        while wait == 1:
+            time.sleep(0.1)
+            try:
+                self.driver.find_element_by_css_selector(self.css["backMain"])
+            except NoSuchElementException:
+                time.sleep(0.1)
+                wait = 0
+
     def reserve(self):
         try:
-            self.wait_element('bookingButton', 'xpath').click()
-            self.wait_element('checkNumber').send_keys(self.data['num'])
-            self.wait_element('sendButton').click()
-        except AttributeError as attrErr:
-            print(attrErr)
+            (self.wait_element('bookingButton', 'css')).click()
+            # self.wait_load()
+            time.sleep(2)
+            (self.wait_element('checkNumber')).send_keys(self.data['num'])
+            (self.wait_element('sendButton')).click()
+        except (AttributeError, ElementNotInteractableException) as except_reserve:
+            print(except_reserve)
             exit("The reserve function error.")
+
+    def reduce_overflow(self, hh, mm, min):
+        hh -= 1
+        mm += 60
+        new_time = datetime.time(hh, mm - min)
+        return new_time.strftime("%H:%M")
+
+    def add_overflow(self, hh, mm, min):
+        hh += 1
+        mm -= 60
+        new_time = datetime.time(hh, mm + min)
+        return new_time.strftime("%H:%M")
+
+    def operation_time(self, hh, mm, min, mode):
+        hh = int(hh)
+        mm = int(mm)
+        if mode == 'reduce':
+            try:
+                new_time = datetime.time(hh, mm - min)
+                return new_time.strftime("%H:%M")
+            except ValueError:
+                return str(self.reduce_overflow(hh, mm, min))
+        elif mode == 'add':
+            try:
+                new_time = datetime.time(hh, mm + min)
+                return new_time.strftime("%H:%M")
+            except ValueError:
+                return str(self.add_overflow(hh, mm, min))
+
+    def reset_time(self, mytime, mode):
+        h, m = mytime.split(':')
+        return self.operation_time(h, m, 15, mode)
+
+    def check_has_car(self, get_car_time, time_data_name):
+        print("== 檢查班次 是否有車班 ==")
+        # regex = re.compile(r'%s \[(有車班|車班已滿\.排候補)\]' % self.data[time_data_name])
+        regex = re.compile(r'%s \[(有車班|車班已滿\.排候補)]' % self.data[time_data_name])
+        car_status = regex.search(get_car_time)
+        # my_debug_log(car_status.groups())
+
+        if car_status.group(1) != '有車班':
+            print(self.data[time_data_name] + "=> 車班已滿")
+            return 0
+        else:
+            print(self.data[time_data_name] + "=> 有車班")
+            return 1
+
+    def go_back_check(self, css_path_name, time_data_name, path_method):
+        change_title = None
+        if time_data_name == 'go_time':
+            change_title = "去程"
+        elif time_data_name == 'back_time':
+            change_title = "回程"
+
+        get_time = self.driver.find_element_by_css_selector("table#innerTable > tbody").text
+        old_time = self.data[time_data_name]
+        count = 0
+        while count < 4:
+            car_result = self.check_has_car(get_time, time_data_name)
+            if car_result == 0:
+                self.data[time_data_name] = self.reset_time(self.data[time_data_name], path_method)
+            else:
+                self.css[css_path_name] = 'input[type=radio][onclick*="%s"]' % self.data[time_data_name]
+                print("更改[%s] %s => [%s]" % (change_title, old_time, self.data[time_data_name]))
+                print("=======================\n")
+                count = 999
+            count += 1
 
     def choose(self):
         try:
-            self.wait_element('dateButton').click()
-            self.wait_element('setGoButton').click()
-            self.wait_element('goTimeButton').click()
-            self.wait_element('setBackButton').click()
-            self.wait_element('backTimeButton').click()
-            self.wait_element('sendButton').click()
-        except AttributeError as attrErr:
-            print(attrErr)
-            exit("The choose function error.")
+            (self.wait_element('dateButton')).click()
+            (self.wait_element('setGoButton')).click()
+            self.go_back_check('goTimeButton', 'go_time', 'reduce')
+            # self.check_enter()
+            (self.wait_element('goTimeButton')).click()
+            (self.wait_element('setBackButton')).click()
+            # self.check_enter()
+            self.go_back_check('backTimeButton', 'back_time', 'add')
+            (self.wait_element('backTimeButton')).click()
+            (self.wait_element('sendButton')).click()
+        finally:
+            pass
+        # except AttributeError as attrErr:
+        #     print(attrErr)
+        #     exit("The choose function error.")
 
     def address(self):
         try:
@@ -158,7 +293,7 @@ class AutoReserve:
             go_on_addr.clear()
             go_on_addr.send_keys(self.data['go_on_address'])
 
-            self.wait_element('goOffAreaIn').click()
+            (self.wait_element('goOffAreaIn')).click()
             Select(self.wait_element('goOffCitySelect')).select_by_value(self.data['go_off_city'])
             Select(self.wait_element('goOffAreaSelect')).select_by_value(self.data['go_off_area'])
             go_off_addr = self.wait_element('goOffAddress')
@@ -169,14 +304,14 @@ class AutoReserve:
             message.clear()
             message.send_keys(self.data['Message'])
 
-            self.wait_element('backOnAreaIn').click()
+            (self.wait_element('backOnAreaIn')).click()
             Select(self.wait_element('backOnCitySelect')).select_by_value(self.data['back_on_city'])
             Select(self.wait_element('backOnAreaSelect')).select_by_value(self.data['back_on_area'])
             back_on_addr = self.wait_element('backOnAddress')
             back_on_addr.clear()
             back_on_addr.send_keys(self.data['back_on_address'])
 
-            self.wait_element('backOffAreaIn').click()
+            (self.wait_element('backOffAreaIn')).click()
             Select(self.wait_element('backOffCitySelect')).select_by_value(self.data['back_off_city'])
             Select(self.wait_element('backOffAreaSelect')).select_by_value(self.data['back_off_area'])
             back_off_addr = self.wait_element('backOffAddress')
@@ -195,7 +330,8 @@ if __name__ == '__main__':
     ycbus = AutoReserve()
     print(u"如果瀏覽器上未使用完成請勿關閉此視窗\n")
     ycbus.login()
-    ycbus.reserve()
+    ycbus.loop_now_time()
+    # ycbus.reserve()
     ycbus.choose()
     ycbus.address()
     print("the %s is finish" % os.path.basename(__file__))
